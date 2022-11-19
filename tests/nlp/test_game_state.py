2c268fb45c0759e448a3760e9f781932373c025c
CalmDownKarm <karmanyaaggarwal@gmail.com>
2022-11-18 21:19:37 -0500

    testing file update

--- tests/nlp/test_game_state.py
+++ tests/nlp/test_game_state.py
@@ -2,7 +2,6 @@ import os
 import gzip
 import json
 import logging
-from itertools import ilen
 from pymongo import MongoClient
 from typing import Iterable
 from pathlib import Path, PosixPath
@@ -41,7 +40,7 @@ def combat_dir_iterator(dirpath: str) -> Iterable[dict]:
             yield from read_gzipped_file(fp)
 
 def find_players(events: Iterable[dict]) -> Iterable[dict]:
-    filtered_events =  filter(lambda event: True if event["event_type"] == "command" and event["command_name"]== "init join" else False, events)
+    filtered_events =  filter(lambda event: True if event["event_type"] == "command" and event.get("command_name", "")== "init join" else False, events)
     for event in filtered_events:
         yield event.get("caster", {})
 
@@ -49,11 +48,12 @@ def dump_players_to_mongo(casters: Iterable[dict]) -> None:
     # establish mongo connection
     collection = "characters"
     for caster in casters:
-        primary_key = {field: caster[field] for field in ["owner", "upstream"]}
-        mongo_db[collection].update_one(primary_key, {"$set": caster}, upsert=True)
+        if caster:
+            primary_key = {field: caster[field] for field in ["owner", "upstream"]}
+            mongo_db[collection].update_one(primary_key, {"$set": caster}, upsert=True)
 
 def find_combat_state_updates(events: Iterable[dict]) -> Iterable[dict]:
-    filtered_events =  filter(lambda event: True if event["event_type"] == "combat_state_update" and event["command_name"]== "init join" else False, events)
+    filtered_events =  filter(lambda event: True if event["event_type"] == "combat_state_update" else False, events)
     for event in filtered_events:
         yield event.get("data", {})
 
@@ -62,20 +62,23 @@ def dump_csu_to_mongo(state_updates: Iterable[dict])-> None:
     TEST_CHANNEL_ID = 314159265358979323  # pi
 
     for state_update in state_updates:
-        state_update["channel"] = TEST_CHANNEL_ID
-        mongo_db[collection].update_one({"channel": TEST_CHANNEL_ID}, {"$set": state_update}, upsert=True)
+        if state_update:
+            state_update["channel"] = TEST_CHANNEL_ID
+            mongo_db[collection].update_one({"channel": TEST_CHANNEL_ID}, {"$set": state_update}, upsert=True)
 
 def test_combat_state_updates():
     combat_state_updates = find_combat_state_updates(combat_dir_iterator("/opt/dataset"))
-    assert ilen(combat_state_updates) > 0
+    assert len(list(combat_state_updates)) > 0
     dump_csu_to_mongo(combat_state_updates)
     collections = list(mongo_db.list_collection_names())
     assert "combats" in collections
+    ghetto_logger(mongo_db['combats'].find({}).limit(5))
+
 
 
 def test_can_reach_dataset():
     players = find_players(combat_dir_iterator("/opt/dataset"))
-    assert ilen(players) > 0
+    assert len(list(players)) > 0
 
 def test_write_players_to_mongo() -> None:
     players = find_players(combat_dir_iterator("/opt/dataset"))
@@ -85,12 +88,12 @@ def test_write_players_to_mongo() -> None:
     assert "characters" in collections
     
 
-def test_avrae_command(avrae_command, avrae, dhttp):
+def test_avrae_command(avrae, dhttp):
     combat_dir = combat_dir_iterator("/opt/dataset")
     players = find_players(combat_dir)
     dump_players_to_mongo(players)
     combat_state_updates = find_combat_state_updates(combat_dir)
     dump_csu_to_mongo(combat_state_updates)
-    avrae.message("Some Command Here")
+    avrae.message("!a mo1")
     dhttp.drain()
 
